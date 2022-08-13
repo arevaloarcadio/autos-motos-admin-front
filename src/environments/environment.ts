ccec7e286c2afc6756c95fc12bdd1cdc6dd27c83
arevaloarcadio.96 <arevaloarcadio.96@gmail.com>
2022-08-13 05:15:27 -0400

    detalles de anuncios individual

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,9 @@
 export const environment:any = {
   production: false,
   //serverUrl: 'http://127.0.0.1:8000/api/auth/',
-  //serverUrl: 'http://178.128.247.56/api/',
-  //server: 'http://178.128.247.56/',
-  serverUrl: 'https://www.backproduction.autosmotos.es/api/',
-  server: 'https://www.backproduction.autosmotos.es/',
+ serverUrl: 'http://localhost:8000/api/',
+  server: 'http://localhost:8000/'
+ // serverUrl: 'https://www.backproduction.autosmotos.es/api/',
+ // server: 'https://www.backproduction.autosmotos.es/',
 
 };
